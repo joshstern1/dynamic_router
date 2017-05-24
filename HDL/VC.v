94beee22fb43c2db1fc599268b0ed0dd32588d76
ycgasjy <ycgasjy@gmail.com>
2017-05-24 18:03:21 -0400

    1

--- HDL/VC.v
+++ HDL/VC.v
@@ -9,5 +9,5 @@ module VC
     output reg [2:0] R, //Route: After routing is completed for a packet, this field holds the output port selected for the packet.
     output reg [3:0] O, //Output VC: After virtual-channel allocation is completed for a packet, this field holds the output virtual channel of port R assigned to the packet.
     output reg [2:0] P, //the number of the empty slots in VC
-    input [2:0] C       //Credit count The number of credits (available downstream flit buffers) for output virtual channel O on output port R.
+    input [2:0] C       //Credit count: The number of credits (available downstream flit buffers) for output virtual channel O on output port R.
 );
